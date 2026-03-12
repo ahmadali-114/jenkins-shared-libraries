f46d340e51b1d72c92d0ef7cb1aa24c2ff565583
Ahmad Ali <113659091+ahmadali-114@users.noreply.github.com>
2026-03-12 16:27:26 +0500

    Refactor dockerpush function parameter names

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,6 +1,7 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-  }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+def call(String project, String imageTag, String dockerHubUser){
+    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+    }
+
+    sh "docker push ${dockerHubUser}/${project}:${imageTag}"
 }
